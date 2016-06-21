210770543ce2ede5ff277dcfc717fb1ff4c926f4
christianaral <christianaral94@gmail.com>
2016-06-21 16:24:30 -0400

    Update tester.js
    added makeMovies()
    added reverseKVpairs()
    fixed getDistribution()
    added example queries for all fn in tester.js

--- js/tester.js
+++ js/tester.js
@@ -2,7 +2,7 @@ function getLongest(property,list) {
     var list = list || allMovies;
     var largest = [0,"property","imdbID"];
     for (m in list) {
-        if (list[m][property].length > largest[0] && list[m].imdbID != "tt3272570") {
+        if (list[m][property].length > largest[0]) {
             largest = [list[m][property].length,
                        list[m][property],
                        list[m].imdbID];
@@ -10,17 +10,18 @@ function getLongest(property,list) {
     }
     return largest;
 }
+// getLongest("Title") // gets the movie with the longest title, its count and imdbID
 
 function getDistribution(property,ignore,list) {
     var ignore = ignore || 0;
     var list = list || allMovies;
-    var types = uniqueList(property);
+    var types = uniqueList(property); // uniqueList(property) is in movies.js
     var obj = {};
     
     for (t in types) {
         var count = 0;
         for (m in list) {
-            if (list[m][property].indepropertyOf(types[t]) != -1) {
+            if (list[m][property].indexOf(types[t]) != -1) {
                 count++;
             }
         }
@@ -35,3 +36,26 @@ function getDistribution(property,ignore,list) {
     
     return obj;
 }
+// getDistribution("Actors",6); // gets an object of people who have been in 6 or more movies
+
+var movies = [];
+function makeMovies(output,IDs,i) {
+    IDs = IDs || allMovies;
+    i = i || 0;
+    var url = "http://www.omdbapi.com/?i="+IDs[i].imdbID+"&plot=short&r=json";
+    $.ajax(url).done(function(data) {
+                     output.push(data);
+                     if (!(typeof IDs[i+1] == "undefined")) {
+                     makeMovies(output,IDs,i+1);
+                     }
+                     });
+}
+// makeMovies(movies);
+
+var rndObj = { fruit:"apple",car:"volvo",tree:"walnut",computer:"mac"}
+function reverseKVpairs(orig) {
+    var y = {};
+    for (i in orig) { y[orig[i]] = i; }
+    return y;
+}
+// rndObj = reverseKVpairs(rndObj);
